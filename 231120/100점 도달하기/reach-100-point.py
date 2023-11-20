8ba28aefa65c5fcb46d04aff616820b6f01d5f8a
yena47 <150795102+yena47@users.noreply.github.com>
2023-11-20 20:03:38 +0900

    Title: 100점 도달하기, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231120/100점 도달하기/reach-100-point.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(n,101,1):
+    if i>=90:
+        print('A',end=' ')
+    elif 90>i>=80:
+        print('B',end=' ')
+    elif 80>i>=70:
+        print('C',end=' ')
+    elif 70>i>=60:
+        print('D',end=' ')
+    else:
+        print('F')
