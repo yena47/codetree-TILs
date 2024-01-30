ba443effae286066df92fed12fda19a179933282
yena47 <150795102+yena47@users.noreply.github.com>
2024-01-30 21:12:07 +0900

    Title: 함수를 이용한 369 게임, Time: 162ms, Memory: 26MB, Status: Passed - CodeTree

--- 240130/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240130/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,13 +1,15 @@
 a,b=map(int,input().split())
 
 
-def fun_369(n):
-    for i in str(n):
-        if int(i)==3 or int(i)==6 or int(i)==9:
+def contains_369(n):
+    while n>0:
+        if n%10==3 or n%10==6 or n%10==9:
             return True
+        n//=10
+    return False
 
 def is_magic_number(n):
-    return n%3==0 or fun_369(n)
+    return n%3==0 or contains_369(n)
 
 
 cnt=0
