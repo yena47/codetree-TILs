cacb15c70acf5f5bcff55914124b764267ba3f9b
yena47 <150795102+yena47@users.noreply.github.com>
2024-03-07 00:54:11 +0900

    Title: 십의 자리 숫자, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,10 @@
+arr=list(map(int,input().split()))
+cnt_arr=[0]*10
+
+# 십의자리수가 3이면 cnt_arr[3]에 1 카운팅하기
+for elem in arr:
+    if elem != 0:
+        cnt_arr[elem//10]+=1
+
+for i in range(1,10):
+    print(f"{i} - {cnt_arr[i]}")
