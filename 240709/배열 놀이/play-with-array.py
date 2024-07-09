a2ce1286c4e98f8f4af200d17641a4c8fdfd412d
yena47 <150795102+yena47@users.noreply.github.com>
2024-07-09 10:15:48 +0900

    Title: 배열 놀이, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240709/배열 놀이/play-with-array.py
+++ 240709/배열 놀이/play-with-array.py
@@ -2,14 +2,14 @@ n,q=map(int,input().split())
 word = list(map(int,input().split()))
 
 for i in range(q):
-    question = list(map(int,input().split()))
+    Q = list(map(int,input().split()))
 
-    if question[0]==1:
-        a=question[1]
+    if Q[0]==1:
+        a=Q[1]
         print(word[a-1])
 
-    elif question[0]==2:
-        b=question[1]
+    elif Q[0]==2:
+        b=Q[1]
         for i in range(n):
             if word[i]==b:
                 print(i+1)
@@ -19,7 +19,7 @@ for i in range(q):
                 
     
     else:
-        s=question[1]
-        e=question[2]
+        s=Q[1]
+        e=Q[2]
         for i in range(s,e+1):
             print(word[i-1],end=' ')
